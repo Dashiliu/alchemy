074da2f0413ccf9c5587ebd098779cb87f3989ad
congbai <congbai@2dfire.com>
2019-05-05 14:38:22 +0800

    ignore target

--- alchemy-benchmarks/target/generated-sources/annotations/com/dfire/platform/alchemy/benchmarks/generated/FlinkEnvironmentContext_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.dfire.platform.alchemy.benchmarks.generated;
-public class FlinkEnvironmentContext_jmhType extends FlinkEnvironmentContext_jmhType_B3 {
-}
-
--- alchemy-benchmarks/target/generated-sources/annotations/com/dfire/platform/alchemy/benchmarks/generated/LogBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.dfire.platform.alchemy.benchmarks.generated;
-public class LogBenchmark_jmhType extends LogBenchmark_jmhType_B3 {
-}
-
